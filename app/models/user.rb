6aca0b9a82013b740515e04ccf7df441c16537e3
Vitalii <turzhansky81@gmail.com>
2020-10-16 21:59:16 +0300

    TM-RG-11: Add model Project

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,4 +30,6 @@ class User < ApplicationRecord
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
+
+  has_many :projects, dependent: :destroy
 end
